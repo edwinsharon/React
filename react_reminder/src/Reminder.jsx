4ecd9c9ec95bd2cf60d97a25897a6a85c34036e7
edwinsharon <edwinmadapallil@gmail.com>
2024-12-18 11:05:23 +0530

    done

--- react_reminder/src/Reminder.jsx
+++ react_reminder/src/Reminder.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reminder.css";
+import { use } from "react";
 function Reminder() {
+  const [Reminder, setReminder] = useState([]);
+  const [newReminder, setnewReminder] = useState("");
+  const handleInputChange = (event) => {
+    setnewReminder(event.target.value);
+  };
+  const handleAddRemider = () => {
+    if (newReminder.trim()) {
+      setReminder([...Reminder, newReminder]);
+      setnewReminder("");
+    }
+  };
+  const handleDelete = (index) => {
+    setReminder(Reminder.filter((item, itemIndex) => itemIndex !== index));
+  };
   return (
     <div className="container">
       <h1>TODO LIST</h1>
       <div className="input-container">
-        <input type="text" />
-        <button>ADD</button>
+        <input
+          type="text"
+          value={newReminder}
+          placeholder="Enter Task"
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddRemider}>ADD</button>
       </div>
+      {Reminder.length > 0 ? (
+        <ul className="reminder-list">
+          {Reminder.map((reminder, index) => (
+            <li key={index} className="reminder-item">
+              <div>{reminder}</div>
+              <button
+                onClick={() => handleDelete(index)}
+                className="delete-button"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Task Found</p>
+      )}
     </div>
   );
 }
